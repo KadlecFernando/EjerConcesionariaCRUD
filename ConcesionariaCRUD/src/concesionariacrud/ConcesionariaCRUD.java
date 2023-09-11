f9d5554550f72c1d58658b8fdee8d194ad8b7c2e
KadlecFernando <kadlec.fernando13@gmail.com>
2023-09-11 14:37:44 -0300

    viewPrincipal

--- ConcesionariaCRUD/src/concesionariacrud/ConcesionariaCRUD.java
+++ ConcesionariaCRUD/src/concesionariacrud/ConcesionariaCRUD.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package concesionariacrud;
 
-/**
- *
- * @author ferge
- */
+import concesionariacrud.vistas.Principal;
+
+
 public class ConcesionariaCRUD {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);   
     }
     
 }
